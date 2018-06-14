58c1b82ec71a00b0a97828142dd89738ca8985ba
JoseMendoza <jose.mendozaar@ug.edu.ec>
2018-06-13 22:45:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/master/curriculumvitae/Conocimientos.java
@@ -0,0 +1,13 @@
+package com.example.master.curriculumvitae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Conocimientos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conocimientos);
+    }
+}
